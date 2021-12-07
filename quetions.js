cdc77d53ab7760b390bb02b97250ce8e9930fda6
sidbhosale1924 <87703375+sidbhosale1924@users.noreply.github.com>
2021-12-07 10:40:48 +0530

    Update quetions.js

--- quetions.js
+++ quetions.js
@@ -3,57 +3,57 @@
 let quetions = [
     {
         numb : 1,
-        quetion : "HTML stand for?",
-        answer : "Hypertext Markup Language",
+        quetion : "Data leakage threats do not usually occur from which of the following?",
+        answer : "Television",
         options : [
-                   "Hypermark Language",
-                   "Hypermix language",
-                   "Hypertext Markup Language",
-                   "Hypertension Language"
+                   "Web and email",
+                   "Mobile data storage",
+                   "USB drives and laptops",
+                   "Television"
                 ]
     },
     {
         numb : 2,
-        quetion : "CSS stand for?",
-        answer : "Cascading style sheet",
+        quetion : "What are the impacts of DLP?",
+        answer : "All of Above",
         options : [
-                   "Common style sheet",
-                   "Colorful style sheet",
-                   "Computer style sheet",
-                   "Cascading style sheet"
+                   "Contractual violation",
+                   "Trust & Relationship impact",
+                   "Monetary penalty ",
+                   "All of Above"
                 ]
     },
     {
         numb : 3,
-        quetion : "PHP Stands for?",
-        answer : "PHP Hypertext Processor",
+        quetion : "What are the reasons of DLP?",
+        answer : "All of Above",
         options : [
-                   "PHP Hypertext Processor",
-                   "PHP Hyper Markup Preprocessor",
-                   "PHP Hyper Markup Processor",
-                   "None of the above"
+                   "Copying data from the client domain",
+                   "Emailing business information to personal email ids",
+                   "Uploading business information / IP on the web",
+                   "All of Above"
                 ]
     },
     {
         numb : 4,
-        quetion : "Which type of language is Javascript?",
-        answer : "Scripting",
+        quetion : "What type of personal information is it okay to share with someone you don't know?",
+        answer : "You should never share ANY personal information with someone you don't know",
         options : [ 
-                   "Programming",
-                   "Scripting",
-                   "Markup",
-                   "None of the above"
+                   "Your name and telephone number only",
+                   "Your email address only, if it doesn't include your name",
+                   "Minor details, like the name of your school and what your parents do for a living",
+                   "You should never share ANY personal information with someone you don't know"
                 ]
     },
     {
         numb : 5,
-        quetion : " From which tag descriptive list starts ?",
-        answer : "DL",
+        quetion : "As part of your current project you need to upload code to customer provided code repository on cloud. Which of the following will you NOT do?",
+        answer : "Upload test data to the cloud repository",
         options : [ 
-                   "LL",
-                   "DL",
-                   "LDL",
-                   "DD"
+                   "Inform the Delivery manager and seek advice",
+                   "Will not upload the test data to the cloud repository.",
+                   "Check for appropriate access to the cloud repository folder",
+                   "Upload test data to the cloud repository"
                 ]
     },
 ];
